62c55513d23472a10a1c78296f23a728a2073b91
kawshikapathum <140903656+kawshikapathum@users.noreply.github.com>
2023-07-29 20:40:55 +0530

    Lab-01

---
+++ Lab 01/practical01/practical01q2/src/main/java/com/mycompany/practical01q2/Practical01q2.java
@@ -0,0 +1,9 @@
+package com.mycompany.practical01q2;
+public class Practical01q2
+{
+public static void main (String [] args)
+{
+System.out.println("Name: Kawshika Pathum");
+System.out.print("Degree program: Software Engineering");
+}
+}
